5129d5eaf0b1e474027626e3e3343d7e422dc763
yutaka2020 <kyutaka2020@gmail.com>
2025-11-05 23:41:04 +0900

    fastAPIとの接続テスト

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 export default function Home() {
   const [imageUrl, setImageUrl] = useState("")
   const [error, setError] = useState("");
-  const [push, setPush] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   // 検索ボタンハ押下時処理
   const handleSearch = async () => {
@@ -15,15 +15,26 @@ export default function Home() {
     }
 
     setError("");
-    setPush(true)
-  }
-  try {
-    const formData = new FormData();
-    formData.append("image_url", imageUrl);
-  }
-  finally {
-  }
+    setLoading(true)
+    try {
+      const formData = new FormData();
+      formData.append("image_url", imageUrl);
 
+      const res = await fetch("http://127.0.0.1:8000/api/search", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await res.json();
+      console.log("サーバーからのレスポンス", data);
+    } catch (err) {
+      console.error("通信エラー", err);
+      setError("サーバーとの通信に失敗しました")
+    } finally {
+      setLoading(false);
+    }
+
+  }
   return (
     <main>
       <h1>AI Search</h1>
@@ -38,7 +49,7 @@ export default function Home() {
       {error && (
         <p>{error}</p>
       )}
-      {imageUrl && push && (
+      {imageUrl && (
         <p>入力値：<b>{imageUrl}</b></p>
       )}
     </main>
