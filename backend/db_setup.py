11d6db0f292d137f5c8223c3da64885187926a75
yutaka2020 <kyutaka2020@gmail.com>
2025-11-16 20:45:40 +0900

    コメントアウトを追加

--- backend/db_setup.py
+++ backend/db_setup.py
@@ -4,10 +4,20 @@ from models import Base
 import os
 from dotenv import load_dotenv
 
+# ===============================
+# DB接続設定
+# ===============================
+
+# .env から環境変数を読み込む
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# SQLAlchemy のエンジンを作成（DB への実際の接続を管理）
 engine = create_engine(DATABASE_URL,future=True)
+# models.pyで定義されたテーブルをDBにを作成
 Base.metadata.create_all(bind=engine)
 
+# SessionLocal: DB セッション（接続）を生成するためのクラス
+# - autocommit=False : 明示的に commit() を呼ぶ必要あり
+# - autoflush=False  : commit前に自動で flush しない
 Session_Local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
